d48e9c6f9d28c1da1049b03aa939916b811dabbf
fitraadinan <fanfitraadina@gmail.com>
2021-06-28 00:06:35 +0700

    revisi2

--- index.php
+++ index.php
@@ -19,25 +19,25 @@
       <i class="fas fa-times" id="cancel"></i>
     </label>
     <div class="sidebar">
-    <img src="GND.png">
-    <ul>
-      <li><a href="#"><i class="fas fa-qrcode"></i>Dashboard</a></li>
-      <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
-      <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
-      <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
-      <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
-    </ul>
+      <img src="GND.png">
+      <ul>
+        <li><a href="#"><i class="fas fa-qrcode"></i>Home</a></li>
+        <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
+        <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
+        <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
+        <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
+      </ul>
     </div>
 
     <div class="navbar">
-    <label for="check">
-      <i id="btn"><img src="GND.png"></i>
-    </label>
-      <ul>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+      <label for="check">
+        <i id="btn"><img src="GND.png"></i>
+      </label>
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
     </div>
 
 <!-- Home -->
@@ -50,18 +50,54 @@
       <p class="typewriter2">My Name is Fitra Adina Nuzulia</p>
     </div>
  </section>
-
+<!-- About -->
 <section id="about" class="about">
-  <div class="container-about">
-    <h2><span>About Us</span></h2>
-  </div>
-  <div class="gatra">
-    <img src="gatra.jpg">
-  </div>
-  <div class="dina">
-    <img src="dina.jpg">
-  </div>
+  <div class="containerdst">
+    <div class="section-title">
+      <h2><span>About Us</span></h2>
+    </div>
 
+    <div class="isiabout">
+      <div class="gatra">
+        <p class="namag">Galih Saputra</p>
+          <img src="gatra.jpg">
+          <ul>
+            <li><i class="fas fa-phone-alt"></i> 085712247539 </li>
+            <li><i class="fas fa-envelope"></i> gatra881@gmail.com</li>
+          </ul>
+          <ul>
+            <li><i class="fas fa-map-marker-alt"></i> Semarang, Indonesia </li>
+            <li><i class="fas fa-calendar-alt"></i> 16 Juny 2000</li>
+          </ul>
+          <p class="aboutg">Balonku ada lima rupa rupa warnanya hijau kuning kelabu merah muda dan biru, meletus balon hijau, DOOORR. Hatiku sangat kacauuu. Balonku tinggal empat ku pegang eraterat</p>
+        
+      </div>
+
+      <p class="namad">Fitra Adina Nuzulia</p>
+
+    <div class="dina">
+      <img src="dina.jpg">
+      <ul>
+        <li><i class="fas fa-map-marker-alt"></i> Semarang, Indonesia </li>
+        <li><i class="fas fa-calendar-alt"></i> 28 November 2001</li>
+      </ul>
+      <ul>
+        <li><i class="fas fa-phone-alt"></i> 0816662367 </li>
+        <li><i class="fas fa-envelope"></i> fanfitraadina@gmail.com</li>
+      </ul>
+      <p class="aboutd">Balonku <b>ada lima rupa rupa warnanya</b> hijau kuning kelabu merah muda dan biru, meletus balon hijau, DOOORR. Hatiku sangat kacauuu. Balonku tinggal empat ku pegang eraterat</p>
+    </div>
+  </div>
+</section>
+
+<!-- SKILL -->
+<section id="skill">
+  <div class="containerdst">
+    <div class="titleskill">
+      <h2><span>Skill Us</span></h2>
+    </div>
+  </div>
+  
 </section>
 
   </body>
