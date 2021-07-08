1f633026f1429cd615df9e6b783a00fe1b455750
fitraadinan <63963463+fitraadinan@users.noreply.github.com>
2021-07-08 14:49:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    
+    <title>Welcome to Gnd Page!</title>
+    <!-- Link CSS Local -->
+    <link href="GND.png" rel="icon">
+    <link rel="stylesheet" href="style.css">
+    <!-- Script JS -->
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <input type="checkbox" id="check">
+    <label for="check">
+      
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+      <img src="GND.png">
+      <ul>
+        <li><a href="#home"><i class="fas fa-qrcode"></i>Home</a></li>
+        <li><a href="#about"><i class="far fa-address-card"></i></i>About</a></li>
+        <li><a href="#skill"><i class="fas fa-users-cog"></i></i>Skills</a></li>
+        <li><a href="#"><i class="fas fa-pencil-ruler"></i></i>Experiences</a></li>
+        <li><a href="#"><i class="fas fa-address-book"></i></i>Contact</a></li>
+      </ul>
+    </div>
+    <div class="navbar">
+      <label for="check">
+        <i id="btn"><img src="GND.png"></i>
+      </label>
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+<!-- Home -->
+ <section id="home">
+    <div class="container">
+      <p class="hi1">Hello,</p>
+      <p class="hi2">allow us to introduce ourselves!</p>
+      <p class="space"></p>
+      <p class="typewriter1">My Name is Galih Saputra</p>
+      <p class="typewriter2">My Name is Fitra Adina Nuzulia</p>
+    </div>
+ </section>
+<!-- About -->
+<section id="about" class="about">
+  <div class="containerdst">
+    <div class="section-title">
+      <h2><span>About Us</span></h2>
+    </div>
+    <div class="isiabout">
+      <div class="gatra">
+        <p class="namag">Galih Saputra</p>
+          <img src="gatra.jpg">
+          <ul>
+            <li><i class="fas fa-phone-alt"></i> <p style="font-weight: bold;">Phone</p> 085712247539 </li>
+            <li><i class="fas fa-envelope"></i> <p style="font-weight: bold;">Email</p> gatra881@gmail.com</li>
+          </ul>
+          <ul>
+            <li><i class="fas fa-map-marker-alt"></i> <p style="font-weight: bold;">Location</p> Semarang, Indonesia </li>
+            <li><i class="fas fa-calendar-alt"></i> <p style="font-weight: bold;">Date of Birthday</p> 16 Juny 2000</li>
+          </ul>
+          <p class="aboutg">Balonku ada lima rupa rupa warnanya hijau kuning kelabu merah muda dan biru, meletus balon hijau, DOOORR. Hatiku sangat kacauuu. Balonku tinggal empat ku pegang eraterat</p>
+        
+      </div>
+      
+      <div class="dina">
+        <p class="namad">Fitra Adina Nuzulia</p>
+        <img src="dina.jpg">
+        <ul>
+          <li><i class="fas fa-map-marker-alt"></i> <p style="font-weight: bold;">Location</p> Semarang, Indonesia </li>
+          <li><i class="fas fa-calendar-alt"></i> <p style="font-weight: bold;">Date of Birthday</p> 28 November 2001</li>
+        </ul>
+        <ul>
+          <li><i class="fas fa-phone-alt"></i> <p style="font-weight: bold;">Phone</p> 0816662367 </li>
+          <li><i class="fas fa-envelope"></i> <p style="font-weight: bold;">Email</p> fanfitraadina@gmail.com</li>
+        </ul>
+        <p class="aboutd">Balonku <b>ada lima rupa rupa warnanya</b> hijau kuning kelabu merah muda dan biru, meletus balon hijau, DOOORR. Hatiku sangat kacauuu. Balonku tinggal empat ku pegang eraterat</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- SKILL -->
+<section id="skill">
+  <div class="containerdst">
+    <div class="titleskill">
+      <h2><span>Skill Us</span></h2>
+    </div>
+
+    <div class="skill">
+      <p class="skillp">Galih Saputra</p>
+      <div class="container-skill">
+        <div class="column">
+          <p>Leadership</p>
+          <div class="soft">
+            <div class="skills lead" style="width: 80%;">80%</div>
+          </div>
+          <p>Team work</p>
+          <div class="soft">
+            <div class="skills team" style="width: 85%;">85%</div>
+          </div>
+          <p>Public Speaking</p>
+          <div class="soft">
+            <div class="skills public" style="width: 75%;">75%</div>
+          </div>
+          <p>Problem Solving</p>
+          <div class="soft">
+            <div class="skills problem" style="width: 75%;">75%</div>
+          </div>
+          <p>Time Management</p>
+          <div class="soft">
+            <div class="skills time" style="width: 60%;">60%</div>
+          </div>
+        </div>
+
+        <div class="column">
+          <p>Nyoba</p>
+        </div>
+        
+      </div>
+    </div>
+
+    <div class="skill">
+      <p class="skillp">Fitra Adina Nuzulia</p>
+      <div class="container-skill">
+        <div class="column">
+          <p>Leadership</p>
+          <div class="soft">
+            <div class="skills lead" style="width: 65%;">65%</div>
+          </div>
+          <p>Team work</p>
+          <div class="soft">
+            <div class="skills team" style="width: 85%;">85%</div>
+          </div>
+          <p>Public Speaking</p>
+          <div class="soft">
+            <div class="skills public" style="width: 60%">60%</div>
+          </div>
+          <p>Problem Solving</p>
+          <div class="soft">
+            <div class="skills problem" style="width: 75%;">75%</div>
+          </div>
+          <p>Time Management</p>
+          <div class="soft">
+            <div class="skills time" style="width: 80%;">80%</div>
+          </div>
+        </div>
+
+        <div class="column">
+          <p>Nyoba</p>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+</section>
+  </body>
+</html>
